ed10a3117cb4aeda563d87f126eef6f463d98f82
unknown <dulb640@EN-284849.UoA.auckland.ac.nz>
2015-09-23 16:57:48 +1200

    added test level, people can take look

---
+++ Phobia/Assets/Scripts/DoorControl.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorControl : MonoBehaviour {
+	
+	public GameObject goalDoor;
+	public GameObject goalRoom;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
