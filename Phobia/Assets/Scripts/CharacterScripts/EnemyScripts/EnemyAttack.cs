a9c59c95ff85480780c0df026508015311770108
Angel <a.tuyay.castro@gmail.com>
2015-10-05 23:10:26 +1300

    Commented Player and Enemy Scripts

--- Phobia/Assets/Scripts/CharacterScripts/EnemyScripts/EnemyAttack.cs
+++ Phobia/Assets/Scripts/CharacterScripts/EnemyScripts/EnemyAttack.cs
@@ -1,25 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/**
+ * 
+ * Class which handles enemy attack logic.
+ * 
+ **/
 public class EnemyAttack : MonoBehaviour
 {
-    
-
     void OnCollisionEnter(Collision other)
     {
-        //if (other.gameObject.CompareTag ("Player")) {
+        // When colliding with player, damage the player.
         HealthControl.dealDamageToPlayer(other.gameObject, 8);
-        //}
     }
 
-
-
-    void onDestroy()
+	void onDestroy()
     {
         Debug.Log("Enemy Destroyed!");
         if (this.tag == "Enemy")
         {
-
+			// When enemy destroyed, increment score.
             TEMPScoreScript.Instance.IncrementScore(10);
         }
     }
