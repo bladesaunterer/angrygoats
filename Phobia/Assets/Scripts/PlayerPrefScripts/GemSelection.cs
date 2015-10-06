f13a9ec6949194e530e649dcc654061b5b867d72
Carson Lourenco <carson.lourenco101@gmail.com>
2015-10-07 09:26:00 +1300

    Changed folder structure

---
+++ Phobia/Assets/Scripts/PlayerPrefScripts/GemSelection.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+//needs test to see if gems can be reset on restart
+public class GemSelection// : PersistentSingleton<GemSelection>
+{
+
+
+	// Use this for initialization
+	public void selectGems (Gem gemOne, Gem gemTwo)
+	{
+		PlayerPrefs.SetString ("GemOne", gemOne.ToString ());
+		PlayerPrefs.SetString ("GemTwo", gemTwo.ToString ());
+	}
+
+	public Gem GetGemOne ()
+	{
+		string gem = PlayerPrefs.GetString ("GemOne");
+		return getEnum (gem);
+	}
+
+	public Gem GetGemTwo ()
+	{
+		string gem = PlayerPrefs.GetString ("GemTwo");
+		return getEnum (gem);
+	}
+
+	private Gem getEnum (string gem)
+	{
+		switch (gem) {
+		case "Yellow":
+			return(Gem.Yellow);
+		case "Blue":
+			return(Gem.Blue);
+		case "Red":
+			return(Gem.Red);
+		case "Green":
+			return(Gem.Green);
+		case "Black":
+			return(Gem.Black);
+		case "White":
+			return(Gem.White);
+		default:
+			return Gem.White;
+		}
+
+	}
+
+}
