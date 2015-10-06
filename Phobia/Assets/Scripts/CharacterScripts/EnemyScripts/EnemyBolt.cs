627ca8507f767b4dfe31dbe7fb859304c5bdfb7e
unknown <fsta657@EN-283900.UoA.auckland.ac.nz>
2015-10-06 18:05:49 +1300

    Forgot to add new files.

---
+++ Phobia/Assets/Scripts/CharacterScripts/EnemyScripts/EnemyBolt.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBolt : MonoBehaviour {
+
+		public float speed;
+		
+		private Rigidbody rb;
+		
+		void Start(){
+			
+			// Get rigidbody and set bolt's velocity.
+			rb = GetComponent<Rigidbody> ();
+			rb.velocity = transform.forward * speed;
+		}
+		
+		void OnTriggerEnter(Collider other)
+		{
+			if (other.gameObject.CompareTag ("Door") 
+			    || other.gameObject.CompareTag ("Wall") 
+			    || other.gameObject.CompareTag ("Enemy") 
+		    || other.gameObject.CompareTag ("Boss") || other.gameObject.CompareTag ("Player")) {
+				
+				// Destroy bolt on contact.
+				Destroy(gameObject);
+				
+				// If bolt hits an enemy, deal damage to that enemy.
+				HealthControl.dealDamageToPlayer(other.gameObject, 50);
+			}
+		}
+	}
---
+++ Phobia/Assets/Scripts/CharacterScripts/EnemyScripts/EnemyPewPew.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyPewPew : MonoBehaviour {
+	public GameObject shot; 
+	public Transform shotSpawn;
+	public Transform shotSpawn1;
+	public Transform shotSpawn2;
+	public Transform shotSpawn3;
+	float timer;
+	private float timeBetweenAttacks = 1f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+		if (timer >= timeBetweenAttacks) {
+			timer = 0f;
+			Instantiate (shot, shotSpawn.position, shotSpawn.rotation);
+			Instantiate (shot, shotSpawn1.position, shotSpawn1.rotation);
+			Instantiate (shot, shotSpawn2.position, shotSpawn2.rotation);
+			Instantiate (shot, shotSpawn3.position, shotSpawn3.rotation);
+		}
+	}
+}
