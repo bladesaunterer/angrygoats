5f30eee9ca4176a2eb6f208bae6e68e889e2684f
bladesaunterer <clou241@EN-284074.UoA.auckland.ac.nz>
2015-10-07 13:15:42 +1300

    bolt will update based on gem selected (with Yubo)

---
+++ Phobia/Assets/BoltRenderer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoltRenderer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Phobia/Assets/Scripts/CharacterScripts/PlayerScripts/BoltMover.cs
+++ Phobia/Assets/Scripts/CharacterScripts/PlayerScripts/BoltMover.cs
@@ -6,34 +6,54 @@ using System.Collections;
  * Class which handles special attack (bolt) logic.
  * 
  **/
-public class BoltMover : MonoBehaviour {
+public class BoltMover : MonoBehaviour
+{
     
-    public float speed;
+	public float speed;
 
 	private Gem gem;
 
 	private Rigidbody rb;
 
-	void Start(){
+	private Gem currentGem;
+	private GameObject gemObject;
 
+	public Renderer renderer;
+	private GemSelection gemSelection = new GemSelection ();
+
+
+
+
+
+
+
+	void Start ()
+	{
+		currentGem = gemSelection.GetCurrentGem ();
+		gemObject = GameObject.FindGameObjectWithTag (currentGem.ToString ());
+		renderer = GetComponent<Renderer> ();
 		// Get rigidbody and set bolt's velocity.
 		rb = GetComponent<Rigidbody> ();
 		rb.velocity = transform.forward * speed;
 
+		GameObject child = this.transform.GetChild (0).gameObject;
+		child.GetComponent<Renderer> ().materials = gemObject.GetComponent<Renderer> ().materials;
+
+
 	}
 
-	void OnTriggerEnter(Collider other)
+	void OnTriggerEnter (Collider other)
 	{
 		if (other.gameObject.CompareTag ("Door") 
-		    || other.gameObject.CompareTag ("Wall") 
-		    || other.gameObject.CompareTag ("Enemy") 
-		    || other.gameObject.CompareTag ("Boss")) {
+			|| other.gameObject.CompareTag ("Wall") 
+			|| other.gameObject.CompareTag ("Enemy") 
+			|| other.gameObject.CompareTag ("Boss")) {
 
 			// Destroy bolt on contact.
-			Destroy(gameObject);
+			Destroy (gameObject);
 
 			// If bolt hits an enemy, deal damage to that enemy.
-			HealthControl.dealDamageToEnemy(other.gameObject);
+			HealthControl.dealDamageToEnemy (other.gameObject);
 		}
 	}
 }
