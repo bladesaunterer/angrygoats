1bc3c2aa2c21e5d7390cbba904e9717bf1de224f
ladiespls <fsta657@aucklanduni.ac.nz>
2015-10-08 08:41:51 +1300

    Added multispawn enemies.

---
+++ Phobia/Assets/Scripts/CharacterScripts/EnemyScripts/EnemyDeathSpawn.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyDeathSpawn : MonoBehaviour {
+
+	public GameObject onDeathCreate;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void OnDestroy() {
+		Vector3 off = new Vector3( 2, 0, 0 );
+		Vector3 notoff = new Vector3( -2, 0, 0 );
+		GameObject make = (GameObject)GameObject.Instantiate (onDeathCreate, this.gameObject.transform.position + off, this.gameObject.transform.rotation);
+		make.GetComponent<AIPath>().target = GameObject.FindWithTag("Player").transform;
+		make = (GameObject) GameObject.Instantiate(onDeathCreate, this.gameObject.transform.position + notoff, this.gameObject.transform.rotation);
+		make.GetComponent<AIPath>().target = GameObject.FindWithTag("Player").transform;
+	}
+}
