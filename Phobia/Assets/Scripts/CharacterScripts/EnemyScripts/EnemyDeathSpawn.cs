dacc67370ecfa2d8ec642b7b8a8d4ce969e2af3c
ladiespls <fsta657@aucklanduni.ac.nz>
2015-10-08 08:54:35 +1300

    Cleans up properly (no more undead creepers)

--- Phobia/Assets/Scripts/CharacterScripts/EnemyScripts/EnemyDeathSpawn.cs
+++ Phobia/Assets/Scripts/CharacterScripts/EnemyScripts/EnemyDeathSpawn.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class EnemyDeathSpawn : MonoBehaviour {
-
+	bool isShuttingDown = false;
 	public GameObject onDeathCreate;
 	// Use this for initialization
 	void Start () {
@@ -10,11 +10,17 @@ public class EnemyDeathSpawn : MonoBehaviour {
 	}
 	
 	void OnDestroy() {
-		Vector3 off = new Vector3( 2, 0, 0 );
-		Vector3 notoff = new Vector3( -2, 0, 0 );
-		GameObject make = (GameObject)GameObject.Instantiate (onDeathCreate, this.gameObject.transform.position + off, this.gameObject.transform.rotation);
-		make.GetComponent<AIPath>().target = GameObject.FindWithTag("Player").transform;
-		make = (GameObject) GameObject.Instantiate(onDeathCreate, this.gameObject.transform.position + notoff, this.gameObject.transform.rotation);
-		make.GetComponent<AIPath>().target = GameObject.FindWithTag("Player").transform;
+		if (!isShuttingDown) {
+			Vector3 off = new Vector3 (2, 0, 0);
+			Vector3 notoff = new Vector3 (-2, 0, 0);
+			GameObject make = (GameObject)GameObject.Instantiate (onDeathCreate, this.gameObject.transform.position + off, this.gameObject.transform.rotation);
+			make.GetComponent<AIPath> ().target = GameObject.FindWithTag ("Player").transform;
+			make = (GameObject)GameObject.Instantiate (onDeathCreate, this.gameObject.transform.position + notoff, this.gameObject.transform.rotation);
+			make.GetComponent<AIPath> ().target = GameObject.FindWithTag ("Player").transform;
+		}
+	}
+
+	void OnApplicationQuit(){
+		isShuttingDown = true;
 	}
 }
