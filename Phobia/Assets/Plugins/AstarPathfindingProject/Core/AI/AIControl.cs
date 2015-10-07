c218aa15d9c6d25eb4ce55777ce8cbf5e095325c
ladiespls <fsta657@aucklanduni.ac.nz>
2015-10-07 18:59:56 +1300

    Added working push enemy and half working flying.

---
+++ Phobia/Assets/Plugins/AstarPathfindingProject/Core/AI/AIControl.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIControl : MonoBehaviour {
+	private bool playerInRange;
+	private GameObject play;
+	private float timeBetweenAttacks = 0.25f;
+	float timer;
+	
+	void Awake ()
+	{
+		playerInRange = false;
+	}
+	void OnTriggerEnter (Collider other)
+	{
+		// When colliding with player, damage the player.
+		if (play == null) {
+			if (other.gameObject.CompareTag("Player")){
+				play = other.gameObject;
+			}
+		}
+		if (other.gameObject.CompareTag ("Player")) {
+			playerInRange = true;
+		}
+	}
+	
+	void OnTriggerExit (Collider other)
+	{
+		if (other.gameObject.CompareTag ("Player")) {
+			playerInRange = false;
+		}
+	}
+	
+	void Update(){
+		timer += Time.deltaTime;
+		if(timer >= timeBetweenAttacks && playerInRange)
+		{
+			Attack (play);
+		}
+	}
+	
+	void Attack(GameObject other)
+	{
+		timer = 0f;
+		if (other != null) {
+			// PUSH!
+		}
+		
+	}
+}
---
+++ Phobia/Assets/Scripts/CharacterScripts/EnemyScripts/EnemyPush.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyPush : MonoBehaviour {
+	public int knockback;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag ("Player")) {
+			if (other.gameObject.GetComponent<Rigidbody>() != null) {
+				Debug.Log("Here!");
+				other.gameObject.GetComponent<Rigidbody>().AddForce(gameObject.transform.forward*knockback);
+			}
+		} 
+	}
+}
