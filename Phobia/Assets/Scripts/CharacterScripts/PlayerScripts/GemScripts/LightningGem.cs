fb6fee602e7910e1354b1aee2eb208cc13180b78
Pietasty <ywu591@aucklanduni.ac.nz>
2015-10-08 14:13:10 +1300

    added lightning gem, commit before refactor

---
+++ Phobia/Assets/Scripts/CharacterScripts/PlayerScripts/GemScripts/LightningGem.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightningGem : GenericGem {
+
+	public int damage = 10;
+	public int duration = 5;
+	public int cost = 25;
+
+	private bool tick;
+	private float endTime;
+	private float nextTime;
+	private AIPath ai;
+
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.K) && playerControl.cooldown >= cost)
+		{
+			playerControl.SubtractCooldown(cost);
+			Instantiate(shot,shotSpawn.position,shotSpawn.rotation);
+		}
+		if (ai != null &&  tick && Time.time > nextTime){
+			nextTime = Time.time + 1f;
+			if (Time.time <= endTime){
+				ai.enabled = false;
+			} else {
+				tick = false;
+				ai.enabled = true;
+			}
+		}
+	}
+
+	public override void onEnemyHit(GameObject other){
+		EnemyHealth health = other.GetComponent<EnemyHealth>();
+		ai = other.GetComponent<AIPath>();
+		if (health != null){
+			health.TakeDamage(damage);
+			tick = true;
+			endTime = Time.time + duration;
+		}
+	}
+}
--- Phobia/Assets/Scripts/CharacterScripts/PlayerScripts/GemSwitching.cs
+++ Phobia/Assets/Scripts/CharacterScripts/PlayerScripts/GemSwitching.cs
@@ -13,7 +13,7 @@ public class GemSwitching : MonoBehaviour
 
 	void Awake ()
 	{
-		GemSelection.selectGems (Gem.Red, Gem.Green);
+		GemSelection.selectGems (Gem.Red, Gem.Yellow);
 
 		spawn = GameObject.FindGameObjectWithTag ("SpecialAttack");
 		gemOne = GameObject.FindGameObjectWithTag (GemSelection.GetGemOne ().ToString ());
