711712aa40d0b8595e28442daeaa3a66326f3dd1
Angel <a.tuyay.castro@gmail.com>
2015-10-10 11:53:02 +1300

    Dialogue text now read from text file.

--- Phobia/Assets/Scripts/CutsceneScripts/CutsceneTextScript.cs
+++ Phobia/Assets/Scripts/CutsceneScripts/CutsceneTextScript.cs
@@ -5,17 +5,24 @@ using System.Collections;
 public class CutsceneTextScript : MonoBehaviour {
 	
 	public Text textBoxString;
+	public TextAsset TextFile;
+	private string[] linesInFile;
+	private int lineNumber;
 
 	void Start() {
-
 		textBoxString = GameObject.FindGameObjectWithTag("SPreLevDialogueText").GetComponentInChildren<Text>();
+		linesInFile = TextFile.text.Split('\n');
+		lineNumber = 0;
 	}
 
 	void Update() {
 		if (Input.GetMouseButtonDown(0)) {
 			Debug.Log("Pressed left click.");
-			textBoxString.text = "PressedLeft.";
 
+			if (lineNumber < linesInFile.Length) {
+				textBoxString.text = linesInFile[lineNumber];
+				lineNumber++;
+			}
 		}
 	}
 }
