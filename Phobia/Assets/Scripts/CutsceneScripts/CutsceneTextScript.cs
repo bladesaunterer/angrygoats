bb7b1bfa4bc4513b2b07341a55803c2dd123aacd
Angel <a.tuyay.castro@gmail.com>
2015-10-10 12:14:38 +1300

    Text now changes color depending on who is speaking.

--- Phobia/Assets/Scripts/CutsceneScripts/CutsceneTextScript.cs
+++ Phobia/Assets/Scripts/CutsceneScripts/CutsceneTextScript.cs
@@ -8,21 +8,32 @@ public class CutsceneTextScript : MonoBehaviour {
 	public TextAsset TextFile;
 	private string[] linesInFile;
 	private int lineNumber;
+	private int y;
 
 	void Start() {
 		textBoxString = GameObject.FindGameObjectWithTag("SPreLevDialogueText").GetComponentInChildren<Text>();
 		linesInFile = TextFile.text.Split('\n');
 		lineNumber = 0;
+		textBoxString.text = linesInFile[lineNumber].Split(':')[1];
+		lineNumber++;
 	}
 
 	void Update() {
 		if (Input.GetMouseButtonDown(0)) {
-			Debug.Log("Pressed left click.");
-
 			if (lineNumber < linesInFile.Length) {
-				textBoxString.text = linesInFile[lineNumber];
+				string[] scriptLine = linesInFile[lineNumber].Split(':');
+				if (scriptLine[0] == "Ndoto") {
+					textBoxString.color = Color.red;
+				} else {
+					textBoxString.color = Color.black;
+				}
+				TypeOutText(scriptLine[1]);
 				lineNumber++;
 			}
 		}
+	}
+
+	private void TypeOutText(string text) {
+		textBoxString.text = text;
 	}
 }
