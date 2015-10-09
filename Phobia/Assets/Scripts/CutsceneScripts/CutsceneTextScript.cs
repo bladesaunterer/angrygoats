eda401a988b1c06d3291c3fda817befba96205dc
Angel <a.tuyay.castro@gmail.com>
2015-10-10 11:37:56 +1300

    Added simple textbox to cutscene, changes on click.

---
+++ Phobia/Assets/Scripts/CutsceneScripts/CutsceneTextScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CutsceneTextScript : MonoBehaviour {
+	
+	public Text textBoxString;
+
+	void Start() {
+
+		textBoxString = GameObject.FindGameObjectWithTag("SPreLevDialogueText").GetComponentInChildren<Text>();
+	}
+
+	void Update() {
+		if (Input.GetMouseButtonDown(0)) {
+			Debug.Log("Pressed left click.");
+			textBoxString.text = "PressedLeft.";
+
+		}
+	}
+}
