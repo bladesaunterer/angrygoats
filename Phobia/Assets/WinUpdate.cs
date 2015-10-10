b3c8afb6c8b2c6604039e5836f7209ec966d19bc
ladiespls <fsta657@aucklanduni.ac.nz>
2015-10-10 14:52:11 +1300

    Updated formatting and dynamic bonus.

--- Phobia/Assets/WinUpdate.cs
+++ Phobia/Assets/WinUpdate.cs
@@ -6,8 +6,40 @@ public class WinUpdate : MonoBehaviour {
 
 	public Text dispText;
 	public void SetFinal(int score, int enemies, int minutes, int seconds){
-		dispText.text = "Grats! You killed " + enemies.ToString() + " enemies in "
-			+ minutes.ToString() +" minutes and " + seconds.ToString() + " seconds, netting you a final score of " + score.ToString() + ".";
+		string second;
+		string minute;
+		int bonus;
+		int final;
+		if (seconds < 10) {
+			second = "0" + seconds.ToString();	
+		} else {
+			second = seconds.ToString();
+		}
+		if (minutes < 10) {
+			minute = "0" + minutes.ToString();	
+		} else {
+			minute = minutes.ToString();
+		}
+
+		if (minutes < 1) {
+			bonus = 500;
+		} else if (minutes < 2) {
+			bonus = 400;
+		} else if (minutes < 3) {
+			bonus = 300;
+		} else if (minutes < 4) {
+			bonus = 200;
+		} else if (minutes < 5) {
+			bonus = 100;
+		} else {
+			bonus = 0;
+		}
+
+		final = bonus + score;
+
+		dispText.text = "WINRAR\n\nEnemies killed : " + enemies.ToString () + " \n(+" + score.ToString () + ")" +
+			"\nTime spent : " + minute + ":" + second + " \n(+" + bonus.ToString() + ")" +
+			"\n\n Total : \t\t " + final.ToString();
 	}
 
 }
