7d9476ec50cbd906fc1ae07fa4e03ec2da76ff1d
Angel <a.tuyay.castro@gmail.com>
2015-10-10 13:21:35 +1300

    Text is now typed out letter by letter.

--- Phobia/Assets/Scripts/CutsceneScripts/CutsceneTextScript.cs
+++ Phobia/Assets/Scripts/CutsceneScripts/CutsceneTextScript.cs
@@ -6,34 +6,43 @@ public class CutsceneTextScript : MonoBehaviour {
 	
 	public Text textBoxString;
 	public TextAsset TextFile;
+	public float textTypingDelay;
+
 	private string[] linesInFile;
+	private string[] scriptLine;
 	private int lineNumber;
-	private int y;
 
 	void Start() {
 		textBoxString = GameObject.FindGameObjectWithTag("SPreLevDialogueText").GetComponentInChildren<Text>();
 		linesInFile = TextFile.text.Split('\n');
 		lineNumber = 0;
-		textBoxString.text = linesInFile[lineNumber].Split(':')[1];
+
+		scriptLine = linesInFile[lineNumber].Split(':');
+		StartCoroutine(TypeOutText(scriptLine[1]));
+
 		lineNumber++;
 	}
 
 	void Update() {
 		if (Input.GetMouseButtonDown(0)) {
 			if (lineNumber < linesInFile.Length) {
-				string[] scriptLine = linesInFile[lineNumber].Split(':');
+				scriptLine = linesInFile[lineNumber].Split(':');
 				if (scriptLine[0] == "Ndoto") {
 					textBoxString.color = Color.red;
 				} else {
 					textBoxString.color = Color.black;
 				}
-				TypeOutText(scriptLine[1]);
+				StartCoroutine(TypeOutText(scriptLine[1]));
 				lineNumber++;
 			}
 		}
 	}
 
-	private void TypeOutText(string text) {
-		textBoxString.text = text;
+	IEnumerator TypeOutText(string lineText) {
+		textBoxString.text = "";
+		foreach (char c in lineText.ToCharArray()) {
+			textBoxString.text += c;
+			yield return new WaitForSeconds(textTypingDelay);
+		}
 	}
 }
