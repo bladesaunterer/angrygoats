502ad046d3256a63ef1b08173bba72391287ea53
IcyCaster <chester.booker.nz@gmail.com>
2015-10-16 03:05:36 +1300

    Playing around with darkness following character

---
+++ Phobia/Assets/Scripts/UIScripts/DarknessFollow.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DarknessFollow : MonoBehaviour
+{
+
+    public GameObject player;
+    public GameObject dark;
+
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+        //offset = dark.transform.position - player.transform.position;
+
+    }
+
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+        //dark.transform.position = player.transform.position + offset;
+    }
+}
