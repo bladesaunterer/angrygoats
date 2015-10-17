485cdaed2db96b2f35de9c5207526678dbe97d4e
zoercai <zoercai@gmail.com>
2015-10-18 11:57:15 +1300

    seeding level generation now working :D

---
+++ Phobia/Assets/Scripts/SeedingScripts/SeedInputProcessor.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SeedInputProcessor : MonoBehaviour {
+
+	public InputField seedField;
+	public Button beginButton;
+	
+	public void Enter (){
+		PlayerPrefs.SetString ("seed", seedField.text);
+	}
+	
+	// Use this for initialization
+	void Start () {
+		beginButton.onClick.AddListener(() => Enter());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Return)){
+			Enter();
+		}
+	}
+}
