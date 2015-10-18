38a7ccb5e478884168fb699a691b59c145bbeea1
Pietasty <ywu591@aucklanduni.ac.nz>
2015-10-18 19:26:38 +1300

    refactored gems and made a template for achievement unlocking

--- Phobia/Assets/Scripts/CharacterScripts/PlayerScripts/GemScripts/AreaOfEffect.cs
+++ Phobia/Assets/Scripts/CharacterScripts/PlayerScripts/GemScripts/AreaOfEffect.cs
@@ -4,21 +4,12 @@ using System.Collections;
 public class AreaOfEffect : GenericGem
 {
 
-	// Use this for initialization
-	
-	// Update is called once per frame
-	void Update ()
-	{
-
-		if (Input.GetKeyDown (KeyCode.K) && playerControl.cooldown >= cost && isCurrent) {
-			playerControl.SubtractCooldown (cost);
-			for (int i=0; i<6; i++) {
-				GameObject shotSpawned = Instantiate (shot, shotSpawn.position, Quaternion.Euler (0, 37 + (shotSpawn.rotation.eulerAngles.y - 15 * i), 0))as GameObject;
-				shotSpawned.GetComponent<BoltMover>().SetGemObject(gameObject);
-			}
-
+	protected override void doEffect(){
+		playerControl.SubtractCooldown (cost);
+		for (int i=0; i<6; i++) {
+			GameObject shotSpawned = Instantiate (shot, shotSpawn.position, Quaternion.Euler (0, 37 + (shotSpawn.rotation.eulerAngles.y - 15 * i), 0))as GameObject;
+			shotSpawned.GetComponent<BoltMover>().SetGemObject(gameObject);
 		}
-	
 	}
 
 	public override void onEnemyHit (GameObject other)
---
+++ Phobia/Assets/Scripts/PlayerPrefScripts/AchievementManager.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AchievementManager : MonoBehaviour {
+
+	private static AchievementManager instance;
+
+	public static AchievementManager Instance{
+		get{
+			if (instance == null)
+			{
+				instance = GameObject.FindObjectOfType<AchievementManager>();
+			}
+			return AchievementManager.instance;
+		}
+	}
+
+	public LevelGenerator generator;
+
+	private bool gemUsage;
+
+	void Start(){
+		gemUsage = false;
+	}
+
+	/**
+	 * Checks for achievements that are earned when you beat a level
+	 */
+	public void OnLevelEnd(){
+		int enemiesCount = TEMPScoreScript.Instance.enemyCounter;
+		int totalEnemies = generator.totalEnemies;
+		if (enemiesCount == 0){
+
+		}
+		if (enemiesCount == totalEnemies){
+
+		}
+		if (!gemUsage){
+
+		}
+	}
+
+	/**
+	 * Checks for achievements when the player dies
+	 */
+	public void OnLevelLoss(){
+		int enemiesCount = TEMPScoreScript.Instance.enemyCounter;
+		if (enemiesCount == 0){
+
+		}
+	}
+
+	/**
+	 * When the user uses a gem
+	 */
+	public void usedGem(){
+		gemUsage = true;
+	}
+}
