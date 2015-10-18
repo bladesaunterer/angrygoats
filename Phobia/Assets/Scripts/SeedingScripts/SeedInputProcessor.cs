ba85839d15f7ce21e7a8718b73ef0f9e35f6cf79
Pietasty <ywu591@aucklanduni.ac.nz>
2015-10-18 20:50:03 +1300

    un-commented code that doesn't work due to unity version

--- Phobia/Assets/Scripts/SeedingScripts/SeedInputProcessor.cs
+++ Phobia/Assets/Scripts/SeedingScripts/SeedInputProcessor.cs
@@ -19,7 +19,7 @@ public class SeedInputProcessor : MonoBehaviour {
 	public InputField maxEnemiesPerRoom;
 	public InputField minWebs;
 	public InputField maxWebs;
-//	public Dropdown levelSelector;
+	public Dropdown levelSelector;
 
 	int actSeedInputInt;
 	int roomsToSpawnInt;
@@ -33,7 +33,7 @@ public class SeedInputProcessor : MonoBehaviour {
 			int.TryParse (totalEnemies.text, out totalEnemiesInt) && int.TryParse (maxEnemiesPerRoom.text, out maxEnemiesPerRoomInt) &&
 			int.TryParse (minWebs.text, out minWebsInt) && int.TryParse (maxWebs.text, out maxWebsInt)) {
 			if (minWebsInt <= maxWebsInt && totalEnemiesInt / roomsToSpawnInt < maxEnemiesPerRoomInt) {
-				/*
+
 				if (levelSelector.value == 0) {
 					print ("Arachnophobia");
 					PlayerPrefs.SetString ("seed", actSeedInput.text + "#" + roomsToSpawn.text + "#" + totalEnemies.text + "#" + maxEnemiesPerRoom.text + "#" + minWebs.text + "#" + maxWebs.text + "#" + "SpiderLevelScene");
@@ -44,7 +44,7 @@ public class SeedInputProcessor : MonoBehaviour {
 					print ("Nyctophobia");
 					PlayerPrefs.SetString ("seed", actSeedInput.text + "#" + roomsToSpawn.text + "#" + totalEnemies.text + "#" + maxEnemiesPerRoom.text + "#" + minWebs.text + "#" + maxWebs.text + "#" + "DarknessLevelScene");
 				} 
-				*/
+
 			}
 		}
 	}
