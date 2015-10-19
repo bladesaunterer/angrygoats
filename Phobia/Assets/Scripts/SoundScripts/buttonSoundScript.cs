e724aa232c797c3cf9f70af568fae038cfe462ed
ladiespls <fsta657@aucklanduni.ac.nz>
2015-10-20 10:24:23 +1300

    Hole plug for sounds crash but still unfixed.

--- Phobia/Assets/Scripts/SoundScripts/buttonSoundScript.cs
+++ Phobia/Assets/Scripts/SoundScripts/buttonSoundScript.cs
@@ -5,18 +5,10 @@ public class buttonSoundScript : MonoBehaviour {
 
 	AudioSource audioSource;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-
 	public void playAudio (string audio){
 		audioSource = GameObject.Find (audio).GetComponent<AudioSource>();
+		if (audioSource != null){
 		audioSource.Play ();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+		}
 	}
 }
