b918c2e8e28f52a3ba129c0a11f9f62db047ba3f
zoercai <zoercai@gmail.com>
2015-10-19 14:15:49 +1300

    UI popups created for endless

---
+++ Phobia/Assets/Scripts/EndlessScripts/IncrementRamp.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IncrementRamp : MonoBehaviour {
+
+	public GameObject ramp;
+	// Use this for initialization
+	void Start () {
+		ramp = GameObject.Find ("Ramp");
+		ramp.GetComponent<Ramp> ().times++;
+	}
+}
