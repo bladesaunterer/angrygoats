14bc5e17b0a80457559e72d5a44fa5dcda0db50a
ladiespls <fsta657@aucklanduni.ac.nz>
2015-10-20 11:15:47 +1300

    Added commenting for all endless bar generator. Issue found to be remedied soon.

--- Phobia/Assets/Scripts/EndlessScripts/IncrementRamp.cs
+++ Phobia/Assets/Scripts/EndlessScripts/IncrementRamp.cs
@@ -1,11 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+/**
+ * Class to handle incrementing number of levels played on the master controller object.
+ * As script to allow for attachment to UI components.
+ */
 public class IncrementRamp : MonoBehaviour {
 
 	public GameObject ramp;
-	// Use this for initialization
 	void Start () {
+		/**
+	 	* Finds the relevant scene object and increments the appropriate field.
+	 	*/
 		ramp = GameObject.Find ("Ramp");
 		ramp.GetComponent<Ramp> ().times++;
 	}
--- Phobia/Assets/Scripts/EndlessScripts/ResetRamp.cs
+++ Phobia/Assets/Scripts/EndlessScripts/ResetRamp.cs
@@ -1,11 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+/**
+ * Class to handle resetting number of levels played on the master controller object.
+ * As script to allow for attachment to UI components.
+ */
 public class ResetRamp : MonoBehaviour {
 	
 	public GameObject ramp;
 	// Use this for initialization
 	void Start () {
+		/**
+	 	* Finds the relevant scene object and increments the appropriate field.
+	 	*/
 		ramp = GameObject.Find ("Ramp");
 		ramp.GetComponent<Ramp> ().times = 0;
 	}
