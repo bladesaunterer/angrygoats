7364e4e930dd736f54b2bf67914d8064eb9bf8eb
zoercai <zoercai@gmail.com>
2015-10-19 23:22:58 +1300

    added button sound script

---
+++ Phobia/Assets/Scripts/SoundScripts/buttonSoundScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class buttonSoundScript : MonoBehaviour {
+
+	AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public void playAudio (string audio){
+		audioSource = GameObject.Find (audio).GetComponent<AudioSource>();
+		audioSource.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
