54dd922494f903b70aaa021c2a13a0f63f88407c
zoercai <zoercai@gmail.com>
2015-10-19 14:16:45 +1300

    endless logic – done with Frano

---
+++ Phobia/Assets/Scripts/EndlessScripts/VampResetScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VampResetScript : MonoBehaviour {
+
+	// AHHHH VAMPIRES! D;
+	public GameObject ramp;
+	// Use this for initialization
+	void Start () {
+		ramp = GameObject.Find ("Ramp");
+		ramp.GetComponent<Ramp> ().times = 0;
+	}
+}
